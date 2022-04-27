d115baf30784019f484c1db38560f9780aa3503e
sjtsh <74091619+sjtsh@users.noreply.github.com>
2022-04-27 13:53:06 +0545

    sdf

--- hotelmanagementbackend/hotelmanagementapp/apis/user.py
+++ hotelmanagementbackend/hotelmanagementapp/apis/user.py
@@ -14,13 +14,8 @@ from hotelmanagementapp.models import *
 @api_view(['POST'])
 def signUp(request):
     if 1 == 1:
-<<<<<<< HEAD
         username = request.data["username"]
         password = request.data["password"]
-=======
-        username =request.data["username"]
-        password =request.data["password"]
->>>>>>> eb86951ac5b7d98a90b7ab01fbd61ba7b4b9760b
         try:
             user = User.objects.get(username=username)
             return Response(False)
@@ -34,12 +29,7 @@ def signIn(request):
     username = request.data["username"]
     password = request.data["password"]
     try:
-<<<<<<< HEAD
-        user = User.objects.get(id=username, password=password)
-        return Response({"username": user.username, "created": user.created})
-=======
         user = User.objects.get(username=username, password = password)
         return Response({"username": user.username, "id": user.id ,"created": user.created})
->>>>>>> eb86951ac5b7d98a90b7ab01fbd61ba7b4b9760b
     except:
         return Response(False)
